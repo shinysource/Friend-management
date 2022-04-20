3eac1eabbc7cea00b50165d6ae194009bb3f41c2
shinysource <highly.flores@gmail.com>
2022-04-20 10:58:25 -0700

    Added auth routes

---
+++ app/routes/auth.routes.js
@@ -0,0 +1,31 @@
+
+const { verifySignUp, authJwt } = require("../middleware");
+const controller = require("../controllers/auth.controller");
+
+module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  })
+
+  app.get(
+    "/api/auth/",
+    authJwt.verifyToken,
+    controller.verify
+  )
+
+  app.post(
+    "/api/auth/signup/",
+    verifySignUp.checkDuplicateUsernameOrEmail,
+    verifySignUp.checkRolesExisted,
+    controller.signup
+  );
+
+  app.post(
+    "/api/auth/signin/",
+    controller.signin
+  );
+};
