8599650f93ee29d95bfe167f61b682708ef4d944
shinysource <highly.flores@gmail.com>
2022-04-20 10:05:55 -0700

    Added user model

---
+++ app/models/user.model.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, Sequelize) => {
+  const User = sequelize.define("User", {
+    username: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    email: {
+      type: Sequelize.STRING,
+      isEmail: true
+    },
+    password: {
+      type: Sequelize.STRING
+    }
+  })
+  return User;
+}
