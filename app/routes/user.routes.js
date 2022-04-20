5ac695c7ee45373d45b3b2982719b32363833ff4
shinysource <highly.flores@gmail.com>
2022-04-20 10:58:44 -0700

    Added user routes

---
+++ app/routes/user.routes.js
@@ -0,0 +1,43 @@
+const { authJwt } = require("../middleware");
+const controller = require("../controllers/user.controller");
+
+module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  app.get(
+    "/api/users/",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    controller.findAll
+  );
+
+  app.get(
+    "/api/users/:id",
+    [authJwt.verifyToken],
+    controller.findOne
+  );
+
+  app.put(
+    "/api/users/:id",
+    [authJwt.verifyToken],
+    controller.update
+  );
+
+  app.delete(
+    "/api/users/:id",
+    [authJwt.verifyToken],
+    controller.delete
+  );
+
+  app.delete(
+    "/api/users/",
+    [authJwt.verifyToken, isAdmin],
+    controller.deleteAll
+  );
+
+};
