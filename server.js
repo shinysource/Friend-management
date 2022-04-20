809585f454a911ffd90253e33ef8c4258228f0eb
shinysource <highly.flores@gmail.com>
2022-04-20 15:57:29 -0700

    Added routes to server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 
 const db = require("./app/models");
+const Role = db.role;
+const User = db.user;
 
 const app = express();
 
@@ -26,8 +28,28 @@ app.get("/", (req, res) => {
 db.sequelize.sync(() => {
 });
 
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+require('./app/routes/friend.routes')(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+
+// Initialize Role models
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
+
+  Role.create({
+    id: 2,
+    name: "admin"
+  });
+
+}
