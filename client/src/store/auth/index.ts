6c5bb25d89544748720b779e3678ac0b1016798f
shinysource <highly.flores@gmail.com>
2022-04-20 20:19:00 -0700

    Added auth store

---
+++ client/src/store/auth/index.ts
@@ -0,0 +1,113 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+
+import { Credentials, SignInfo, AuthState, AuthResult } from './types'
+
+import api from 'services/api'
+
+const initialState: AuthState = {
+  user: {
+    id: 0,
+    username: '',
+    email: '',
+    role: '',
+    password: ''
+  },
+  errors: '',
+  loading: false,
+  verified: false,
+  checked: false
+}
+
+export const verify = createAsyncThunk('auth/verify', async () => {
+  try {
+    const response = await api.verify()
+    return response.data
+  } catch (error) {
+    const err = error as any
+    throw err.response?.data
+  }
+})
+
+export const signup = createAsyncThunk(
+  'auth/signup',
+  async (credentials: Required<Credentials>) => {
+    try {
+      const response = await api.signup(credentials)
+      return response.data
+    } catch (error) {
+      const err = error as any
+      throw err.response?.data
+    }
+  }
+)
+
+export const signin = createAsyncThunk(
+  'auth/signin',
+  async (signinInfo: Required<SignInfo>, { rejectWithValue }) => {
+    try {
+      const response = await api.signin(signinInfo)
+      return response.data
+    } catch (error) {
+      const err = error as any
+      throw rejectWithValue(err.response?.data)
+    }
+  }
+)
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    signout: (state) => {
+      localStorage.removeItem('toekn')
+      state.user = initialState.user
+      state.checked = false
+      state.loading = false
+      state.verified = false
+    },
+    setChecked: (state, action) => {
+      state.checked = action.payload
+    },
+    setVerified: (state, action) => {
+      state.verified = action.payload
+    }
+  },
+  extraReducers(builder): void {
+    builder
+      .addCase(verify.pending, (state) => {
+        state.loading = true
+      })
+      .addCase(verify.fulfilled, (state, action) => {
+        state.loading = false
+        state.checked = true
+        state.verified = true
+        const { data } = action.payload
+        state.user = data.user
+      })
+      .addCase(verify.rejected, (state) => {
+        state.loading = false
+        state.checked = false
+        state.verified = true
+      })
+      .addCase(signup.pending, (state) => {
+        state.loading = true
+      })
+      .addCase(signup.fulfilled, (state, action) => {
+        state.loading = false
+        state.verified = true
+        const { data } = action.payload
+        state.user = data.user
+        localStorage.setItem('token', data.token)
+      })
+      .addCase(signup.rejected, (state, action) => {
+        state.loading = false
+        state.verified = true
+      })
+  }
+})
+
+export const signout = authSlice.actions.signout
+export const setChecked = authSlice.actions.setChecked
+export const setVerified = authSlice.actions.setVerified
+
+export default authSlice.reducer
---
+++ client/src/store/auth/types.ts
@@ -0,0 +1,34 @@
+export interface Credentials {
+  username: string
+  email: string
+  password: string
+}
+
+export interface SignInfo {
+  email: string
+  password: string
+}
+
+export interface User {
+  id: number
+  username: string
+  email: string
+  password: string
+  role: string
+}
+
+export interface AuthState {
+  user: User
+  errors?: string
+  loading: boolean
+  verified: boolean
+  checked: boolean
+}
+
+export interface AuthResult {
+  data: {
+    user: User
+    token: string
+    message: string
+  }
+}
