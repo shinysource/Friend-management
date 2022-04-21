30b0b7b713d05ebca475ba67cc12d078dd32519e
shinysource <highly.flores@gmail.com>
2022-04-20 20:20:06 -0700

    Added provider to app

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,19 @@
 import { StrictMode } from 'react'
 import { render } from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './store/store'
 import App from 'App'
 
 import 'styles/tailwind.css'
 
 render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
   document.getElementById('root')
 )
