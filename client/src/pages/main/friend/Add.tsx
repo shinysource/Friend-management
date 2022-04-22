2e2f1a3f138be8b9d3f9066949e8dd3842a617f3
shinysource <highly.flores@gmail.com>
2022-04-21 19:06:52 -0700

    Added friend add page

--- client/src/pages/main/friend/Add.tsx
+++ client/src/pages/main/friend/Add.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 import { toast } from 'react-toastify'
 
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
 
-import { Grid } from '@mui/material'
+import { Grid, Container } from '@mui/material'
 import PlusOneIcon from '@mui/icons-material/PlusOne'
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew'
 import {
@@ -15,9 +14,16 @@ import {
   FormControl,
   FormLabel
 } from '@mui/material'
+import Divider from '@mui/material/Divider'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Avatar from '@mui/material/Avatar'
+import { deepOrange, green } from '@mui/material/colors'
 
 import CustomButton from 'components/Button/CustomButton'
 import FormInput from 'components/Fields/FormInput'
+import FormSelect from 'components/Fields/FormMultiSelect'
+import CustomBreadcrumbs from 'components/Breadcrumbs/CustomBreadcrumbs'
 
 import { useAppDispatch, useAppSelector } from 'store/hooks'
 import { RootState } from 'store/store'
@@ -29,6 +35,14 @@ const validationSchema = Yup.object().shape({
   friendname: Yup.string().required('Enter your username'),
   email: Yup.string().required('Enter your Email').email('Enter a valid Email')
 })
+
+const hobbies = [
+  { value: 'pingpong', label: 'Pingpong' },
+  { value: 'pingpong', label: 'Basketball' },
+  { value: 'pingpong', label: 'Valleyball' },
+  { value: 'pingpong', label: 'Football' },
+  { value: 'pingpong', label: 'Reading' }
+]
 
 interface FriendForm {
   friendname: string
@@ -42,7 +56,7 @@ interface FriendForm {
 const initialValues: FriendForm = {
   friendname: '',
   email: '',
-  gender: '',
+  gender: 'female',
   age: 0,
   hobbies: '',
   description: ''
@@ -74,163 +88,189 @@ const Add = () => {
     }
   })
 
-  const handleChange = () => {}
-
   return (
     <>
       <Header />
-      <Grid
-        container
-        justifyContent="center"
-        spacing={5}
-        py={6}
-        className="min-h-screen"
-      >
-        <Grid
-          item
-          container
-          xs={12}
-          justifyContent="center"
-          alignItems="center"
-        >
-          <div className="flex justify-center">
-            <p className="font-podium49 text-4xl uppercase text-grey">{`${user.username}`}</p>
-            <p className="font-podium49 text-4xl uppercase">'s New Friend</p>
-          </div>
+      <Container>
+        <Grid container>
+          <Grid item py={2}>
+            <CustomBreadcrumbs />
+          </Grid>
         </Grid>
+        <Divider />
 
-        <Grid item xs={12}>
-          <form onSubmit={formik.handleSubmit} className="flex justify-center">
-            <Grid
-              item
-              className="lg:!max-w-[1800px]"
-              container
-              justifyContent="center"
-              spacing={2}
-              xs={10}
-              sm={8}
-              md={6}
-              lg={4}
-            >
-              <Grid item container justifyContent="space-between">
-                <Grid item xs={4}>
-                  <Link to="/friend">
-                    <CustomButton
-                      type="button"
-                      model="primary"
-                      variant="contained"
-                      label="Back"
-                      startIcon={
-                        <ArrowBackIosNewIcon
-                          sx={{ fontSize: '14px !important' }}
+        <Grid pt={2}></Grid>
+        <Card>
+          <CardContent>
+            <Grid container py={2} spacing={3}>
+              <Grid item xs={12} md={4}>
+                <Card variant="outlined">
+                  <CardContent>
+                    <Avatar
+                      alt={user.username}
+                      src="/broken-image.jpg"
+                      sx={{
+                        bgcolor: green[500],
+                        width: 250,
+                        height: 250,
+                        fontSize: 200
+                      }}
+                      className="mx-auto uppercase"
+                    />
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid
+                item
+                container
+                justifyContent="center"
+                xs={12}
+                md={8}
+                spacing={4}
+              >
+                <Grid item xs={12}>
+                  <div className="flex justify-center">
+                    <p className="font-podium49 text-4xl uppercase text-grey">{`${user.username}`}</p>
+                    <p className="font-podium49 text-4xl uppercase">
+                      's New Friend
+                    </p>
+                  </div>
+                </Grid>
+
+                <Grid item xs={10} className="!mx-auto">
+                  <form onSubmit={formik.handleSubmit}>
+                    <Grid
+                      item
+                      container
+                      justifyContent="center"
+                      spacing={2}
+                      xs={12}
+                    >
+                      <Grid item xs={12}>
+                        <FormInput
+                          id="friendname"
+                          name="friendname"
+                          formik={formik}
+                          handleChange={formik.handleChange}
+                          className="font-inter text-base font-normal"
+                          label="Name"
+                          placeholder="Name"
+                          isHint={true}
                         />
-                      }
-                    />
-                  </Link>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <FormInput
+                          id="email"
+                          type="email"
+                          name="email"
+                          formik={formik}
+                          handleChange={formik.handleChange}
+                          className="font-inter text-base font-normal"
+                          label="Email"
+                          placeholder="Email"
+                          isHint={true}
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <FormControl>
+                          <FormLabel id="demo-radio-buttons-group-label">
+                            Gender
+                          </FormLabel>
+                          <RadioGroup
+                            aria-labelledby="demo-radio-buttons-group-label"
+                            name="gender"
+                            value={formik.values['gender']}
+                            onChange={formik.handleChange}
+                            defaultValue="female"
+                            row
+                          >
+                            <FormControlLabel
+                              value="female"
+                              control={<Radio />}
+                              label="Female"
+                            />
+                            <FormControlLabel
+                              value="male"
+                              control={<Radio />}
+                              label="Male"
+                            />
+                          </RadioGroup>
+                        </FormControl>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <FormInput
+                          type="number"
+                          id="age"
+                          name="age"
+                          formik={formik}
+                          handleChange={formik.handleChange}
+                          className="font-inter text-base font-normal"
+                          label="Age"
+                          placeholder="Age"
+                          isHint={true}
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <FormSelect
+                          id="hobbies"
+                          name="hobbies"
+                          options={hobbies}
+                          label="Hobby"
+                          placeholder="Select your country"
+                          multiple={true}
+                          formik={formik}
+                          // handleChange={formik.handleChange}
+                          isHint={true}
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <FormInput
+                          type="text"
+                          id="description"
+                          name="description"
+                          formik={formik}
+                          handleChange={formik.handleChange}
+                          className="font-inter text-base font-normal"
+                          label="Description"
+                          placeholder="Description"
+                          isHint={true}
+                        />
+                      </Grid>
+                      <Grid item container justifyContent="space-between">
+                        <Grid item xs={4}>
+                          <Link to="/friend">
+                            <CustomButton
+                              type="button"
+                              model="primary"
+                              variant="contained"
+                              label="Back"
+                              startIcon={
+                                <ArrowBackIosNewIcon
+                                  sx={{ fontSize: '14px !important' }}
+                                />
+                              }
+                            />
+                          </Link>
+                        </Grid>
+                        <Grid item xs={4}>
+                          <CustomButton
+                            type="submit"
+                            model="primary"
+                            variant="contained"
+                            label="ADD Friend"
+                            loading={loading}
+                            startIcon={<PlusOneIcon fontSize="large" />}
+                          />
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                  </form>
                 </Grid>
-                <Grid item xs={4}>
-                  <CustomButton
-                    type="submit"
-                    model="primary"
-                    variant="contained"
-                    label="ADD Friend"
-                    loading={loading}
-                    startIcon={<PlusOneIcon fontSize="large" />}
-                  />
-                </Grid>
-              </Grid>
-              <Grid item xs={12}>
-                <FormInput
-                  id="friendname"
-                  name="friendname"
-                  formik={formik}
-                  handleChange={formik.handleChange}
-                  className="font-inter text-base font-normal"
-                  label="Name"
-                  placeholder="Name"
-                  isHint={true}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormInput
-                  id="email"
-                  type="email"
-                  name="email"
-                  formik={formik}
-                  handleChange={formik.handleChange}
-                  className="font-inter text-base font-normal"
-                  label="Email"
-                  placeholder="Email"
-                  isHint={true}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControl>
-                  <FormLabel id="demo-radio-buttons-group-label">
-                    Gender
-                  </FormLabel>
-                  <RadioGroup
-                    aria-labelledby="demo-radio-buttons-group-label"
-                    defaultValue="female"
-                    name="gender"
-                    row
-                  >
-                    <FormControlLabel
-                      value="female"
-                      control={<Radio />}
-                      label="Female"
-                    />
-                    <FormControlLabel
-                      value="male"
-                      control={<Radio />}
-                      label="Male"
-                    />
-                  </RadioGroup>
-                </FormControl>
-              </Grid>
-              <Grid item xs={12}>
-                <FormInput
-                  type="number"
-                  id="age"
-                  name="age"
-                  formik={formik}
-                  handleChange={formik.handleChange}
-                  className="font-inter text-base font-normal"
-                  label="Age"
-                  placeholder="Age"
-                  isHint={true}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormInput
-                  type="text"
-                  id="hobbies"
-                  name="hobbies"
-                  formik={formik}
-                  handleChange={formik.handleChange}
-                  className="font-inter text-base font-normal"
-                  label="Hobbies"
-                  placeholder="Hobbies"
-                  isHint={true}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormInput
-                  type="text"
-                  id="description"
-                  name="description"
-                  formik={formik}
-                  handleChange={formik.handleChange}
-                  className="font-inter text-base font-normal"
-                  label="Description"
-                  placeholder="Description"
-                  isHint={true}
-                />
               </Grid>
             </Grid>
-          </form>
-        </Grid>
-      </Grid>
+          </CardContent>
+        </Card>
+      </Container>
     </>
   )
 }
