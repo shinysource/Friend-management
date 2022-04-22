240f458179519bb268456a45de5281d77291db03
shinysource <highly.flores@gmail.com>
2022-04-22 07:26:59 -0700

    Updated friend list page

--- client/src/pages/main/friend/Friend.tsx
+++ client/src/pages/main/friend/Friend.tsx
@@ -2,12 +2,25 @@ import React, { useEffect } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 import { toast } from 'react-toastify'
 
-import { Grid } from '@mui/material'
-
+import { Grid, Container } from '@mui/material'
+import Divider from '@mui/material/Divider'
+import {
+  DataGrid,
+  GridRenderCellParams,
+  GridColDef,
+  GridValueGetterParams,
+  GridActionsCellItem
+} from '@mui/x-data-grid'
+import Card from '@mui/material/Card'
+import CardHeader from '@mui/material/CardHeader'
+import CardContent from '@mui/material/CardContent'
 import PlusOneIcon from '@mui/icons-material/PlusOne'
+import EditIcon from '@mui/icons-material/Edit'
+import DeleteIcon from '@mui/icons-material/DeleteOutlined'
 
 import CustomButton from 'components/Button/CustomButton'
 import FormInput from 'components/Fields/FormInput'
+import CustomBreadcrumbs from 'components/Breadcrumbs/CustomBreadcrumbs'
 
 import { useAppDispatch, useAppSelector } from '../../../store/hooks'
 import { RootState } from '../../../store/store'
@@ -19,7 +32,7 @@ const Friend = () => {
   const navigate = useNavigate()
   const dispatch = useAppDispatch()
   const { user } = useAppSelector((state: RootState) => state.auth)
-  const { friends, loading, updated } = useAppSelector(
+  const { friend, loading, updated } = useAppSelector(
     (state: RootState) => state.friend
   )
 
@@ -37,6 +50,56 @@ const Friend = () => {
     [dispatch]
   )
 
+  const handleEdit = (id: number) => {
+    navigate('/friend/edit/' + id)
+  }
+
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'Id', flex: 1 },
+    { field: 'friendname', headerName: 'Name', flex: 2 },
+    { field: 'email', headerName: 'Email', flex: 3 },
+    { field: 'gender', headerName: 'Gender', flex: 1 },
+    {
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+      flex: 1
+    },
+    {
+      field: 'hobbies',
+      headerName: 'Hobbies',
+      flex: 4
+    },
+    {
+      field: 'description',
+      headerName: 'Description',
+      flex: 4
+    },
+    {
+      field: 'actions',
+      headerName: 'Actions',
+      flex: 2,
+      align: 'center',
+      renderCell: (params: GridRenderCellParams) => (
+        <>
+          <GridActionsCellItem
+            icon={<EditIcon />}
+            label="Edit"
+            className="textPrimary"
+            onClick={() => handleEdit(params.row.id)}
+            color="inherit"
+          />
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={() => handleDelete(params.row.id)}
+            color="inherit"
+          />
+        </>
+      )
+    }
+  ]
+
   useEffect(() => {
     dispatch(getFriendByUserId(user.id))
       .unwrap()
@@ -51,36 +114,64 @@ const Friend = () => {
   return (
     <>
       <Header />
-      <Grid container justifyContent="center" spacing={5} py={6}>
-        <Grid item xs={12}>
-          <div className="flex justify-center">
-            <p className="font-podium49 text-4xl uppercase text-grey">{`${user.username}`}</p>
-            <p className="font-podium49 text-4xl uppercase">'s Friends</p>
-          </div>
-        </Grid>
-        <Grid item container sm={11} lg={10} justifyContent="space-between">
-          <Grid item xs={2}>
-            <Link to="/friend/add">
-              <CustomButton
-                type="button"
-                model="primary"
-                variant="contained"
-                label="ADD Friend"
-                startIcon={<PlusOneIcon fontSize="large" />}
-              />
-            </Link>
-          </Grid>
-          <Grid item xs={2}>
-            <FormInput
-              type="text"
-              name="friendname"
-              handleChange={handleChange}
-              label="Search your friend"
-              placeholder="Name and Email"
-            />
+      <Container>
+        <Grid container>
+          <Grid item py={2}>
+            <CustomBreadcrumbs />
           </Grid>
         </Grid>
-      </Grid>
+
+        <Grid pt={2}></Grid>
+        <Card>
+          <CardHeader>
+            <Grid item xs={12}>
+              <div className="flex justify-center">
+                <p className="font-podium49 text-4xl uppercase text-grey">{`${user.username}`}</p>
+                <p className="font-podium49 text-4xl uppercase">
+                  's New Friend
+                </p>
+              </div>
+            </Grid>
+          </CardHeader>
+          <CardContent>
+            <Grid item container sm={11} lg={10} justifyContent="space-between">
+              <Grid item xs={2}>
+                <Link to="/friend/add">
+                  <CustomButton
+                    type="button"
+                    model="primary"
+                    variant="contained"
+                    label="ADD Friend"
+                    startIcon={<PlusOneIcon fontSize="large" />}
+                  />
+                </Link>
+              </Grid>
+              <Grid item xs={2}>
+                <FormInput
+                  type="text"
+                  name="friendname"
+                  handleChange={handleChange}
+                  label="Search your friend"
+                  placeholder="Name and Email"
+                />
+              </Grid>
+            </Grid>
+            <Grid container py={2} spacing={3}>
+              <Grid item xs={12}>
+                <DataGrid
+                  autoHeight
+                  {...friend}
+                  pageSize={5}
+                  rowsPerPageOptions={[5]}
+                  rows={friend}
+                  columns={columns}
+                  loading={loading}
+                />
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+      </Container>
     </>
   )
 }
