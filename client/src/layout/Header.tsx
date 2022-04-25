bff151aaa5e00fe27e90c3a2ded526c27b7db75e
shinysource <highly.flores@gmail.com>
2022-04-24 17:16:13 -0700

    Fixed header avatar

--- client/src/layout/Header.tsx
+++ client/src/layout/Header.tsx
@@ -129,7 +129,11 @@ const Header = () => {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar
+                  alt={user.username}
+                  src="/static/images/avatar/2.jpg"
+                  className="uppercase"
+                />
               </IconButton>
             </Tooltip>
             <Menu
